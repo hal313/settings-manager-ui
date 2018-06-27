2a47f44f6066ea025bf68d35f8375b4d5a587df5
John Ghidiu <jghidiu@solutechnology.com>
2018-06-27 16:58:00 -0400

    Added comment

--- src/util/getTypeFromElement.js
+++ src/util/getTypeFromElement.js
@@ -1,8 +1,9 @@
 // TODO: Test
 import { Constants } from '../Constants.js';
+import { getTypeFromValue } from './getTypeFromValue.js';
 
 let getTypeFromElement = (element) => {
-    return element.getAttribute(Constants.ATTRIBUTE_TYPE);
+    return element.getAttribute(Constants.ATTRIBUTE_TYPE);// || getTypeFromValue(element.value);
 };
 
 export {getTypeFromElement};
