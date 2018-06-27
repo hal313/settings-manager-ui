b0a4c4496b11f5cc4e3b4b38c45208a10db76c29
John Ghidiu <jghidiu@solutechnology.com>
2018-06-26 23:42:17 -0400

    Changed "objectarray" type to "collection:object"

--- src/util/getTypeFromValue.js
+++ src/util/getTypeFromValue.js
@@ -3,7 +3,7 @@ import { isArray } from './isArray.js';
 let getTypeFromValue = (value) => {
     // Because this module's internal types mirror that of JavaScript (with the exception of objectarray),
     // it is necessary only have to check for object array and return the JavaScript type otherwise
-    return isArray(value) ? 'objectarray' : typeof value;
+    return isArray(value) ? 'collection:object' : typeof value;
 };
 
 export {getTypeFromValue};
