eaa005338e83c6a82ca28ea8a24916694efa31d3
John Ghidiu <jghidiu@solutechnology.com>
2018-06-29 16:01:39 -0400

    Made functions truly public

--- src/util/TypeDecoratorManager.js
+++ src/util/TypeDecoratorManager.js
@@ -1,47 +1,58 @@
 import { asArray } from './asArray.js';
 
+// TODO: Write tests
 export class TypeDecoratorManager {
 
-    // TODO: Write tests
-    // TODO: Pass in flat-name during apply
     constructor() {
+        this.typeDecorators = {};
+    }
 
-        let typeDecorators = {};
-        /**
-         * Adds decorators for a type.
-         *
-         * @param {String} type the type to add decorators for
-         * @param {Function | Function[]} decoratorFns the decorator functions
-         */
-        this.addTypeDecorators = (type, decoratorFns) => {
-            if (!typeDecorators[type]) {
-                typeDecorators[type] = [];
-            }
-
-            asArray(decoratorFns).forEach((decoratorFn) => {
-                typeDecorators[type].push(decoratorFn);
-            });
-        };
-
-        this.getTypeDecorators = (type) => {
-            let decorators = [];
-
-            Object.keys(typeDecorators).forEach((pattern) => {
-                if (null !== type.match(pattern)) {
-                    decorators = decorators.concat(typeDecorators[pattern]);
-                }
-            });
-
-            return decorators;
-        };
-
-        this.applyTypeDecorators = (type, element, name, value) => {
-            this.getTypeDecorators(type).forEach((decorate) => {
-                decorate(element, type, name, value);
-            });
-            return element;
+    /**
+     * Adds decorators for a type.
+     *
+     * @param {String} type the type to add decorators for
+     * @param {Function | Function[]} decoratorFns the decorator functions
+     */
+    addTypeDecorators(type, decoratorFns) {
+        if (!this.typeDecorators[type]) {
+            this.typeDecorators[type] = [];
         }
 
+        asArray(decoratorFns).forEach((decoratorFn) => {
+            this.typeDecorators[type].push(decoratorFn);
+        });
+    };
+
+    /**
+     * Gets the type decorator functions registered for the specified type.
+     *
+     * @param {String} type the type to get the decorators for
+     * @return {Function[]} the decorator functions registered for the type
+     */
+    getTypeDecorators(type) {
+        let decorators = [];
+
+        Object.keys(this.typeDecorators).forEach((pattern) => {
+            if (null !== type.match(pattern)) {
+                decorators = decorators.concat(this.typeDecorators[pattern]);
+            }
+        });
+
+        return decorators;
+    };
+
+    /**
+     *
+     * @param {String} type the type to get the decorators for
+     * @param {Element} element the element to operate on
+     * @param {String} name the name of the value
+     * @param {*} value the value assigned to the element
+     */
+    applyTypeDecorators(type, element, name, value) {
+        this.getTypeDecorators(type).forEach((decorate) => {
+            decorate(element, type, name, value);
+        });
+        return element;
     }
 
 };
