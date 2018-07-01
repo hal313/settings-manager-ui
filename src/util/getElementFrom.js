6ceff358fa8f994794f22d1f1a05e85dec25c8f4
John Ghidiu <jghidiu@solutechnology.com>
2018-07-01 15:19:28 -0400

    Removed obsolete file

--- src/util/getElementFrom.js
+++
@@ -1,8 +0,0 @@
-// import { isElement } from './isElement';
-// import { getOneElement } from './getOneElement';
-
-// let getElementFrom = (target) => {
-//     return isElement(target) ? target : getOneElement(target, document);
-// };
-
-// export {getElementFrom};
