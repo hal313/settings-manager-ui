246b66ed9832f0c69c9ebf4e52d1289b219856d1
John Ghidiu <jghidiu@solutechnology.com>
2018-07-01 15:51:35 -0400

    Fixed documentation

--- src/util/getOneElement.js
+++ src/util/getOneElement.js
@@ -8,7 +8,7 @@ import { isDefined } from './isDefined.js';
  * Returns exactly one element from the search. If 0 or multiple elements are
  * found, then an error is thrown.
  *
- * @param {Element|String} target the target to get one element from
+ * @param {Element|String} target the target to get one element from (either an Element or an css selector)
  * @param {Element} [rootElement] the element to start the search from (defautls to document.body)
  * @returns {Element} the only element which matches the search, rooted from the rootElement
  * @throws when the type is wrong or 0 or more than 1 elements are found
