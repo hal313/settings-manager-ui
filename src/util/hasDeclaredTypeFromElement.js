9f397e4e50b8ebc6af5a75533f5ad238f3629b01
John Ghidiu <jghidiu@solutechnology.com>
2018-07-02 19:30:13 -0400

    Added hasDeclaredTypeFromElement

---
+++ src/util/hasDeclaredTypeFromElement.js
@@ -0,0 +1,25 @@
+import { createError } from './createError.js';
+import { isElement } from './isElement.js';
+import { Constants } from '../Constants.js';
+import { isString } from './isString.js';
+import { isEmpty } from './isEmpty.js';
+import { isDefinedAndNotEmpty } from './isDefinedAndNotEmpty.js';
+
+/**
+ * Determines if an element has a declared type.
+ *
+ * @param {Element} element the element to check
+ * @returns {Boolean} true if the element has a declared type
+ */
+let hasDeclaredTypeFromElement = (element) => {
+    if (!isElement(element)) {
+        throw createError('"element" must be an Element');
+    }
+
+    // Check declared type
+    let type = element.getAttribute(Constants.ATTRIBUTE_TYPE);
+
+    return isDefinedAndNotEmpty(type);
+};
+
+export {hasDeclaredTypeFromElement};
---
+++ test/spec/util/hasDeclaredTypeFromElement.spec.js
@@ -0,0 +1,45 @@
+import { hasDeclaredTypeFromElement } from '../../../src/util/hasDeclaredTypeFromElement.js';
+import { asElement } from '../../../src/util/asElement.js';
+import { Constants } from '../../../src/Constants.js';
+
+describe('hasDeclaredTypeFromElement', () => {
+
+    describe('Lifecycle', () => {
+
+        test('exists as a function', () => {
+            expect(hasDeclaredTypeFromElement).toEqual(expect.any(Function));
+        });
+
+    });
+
+    describe('API', function () {
+
+        [true, false, -1, 0, 1, NaN, '', 'some string', null, undefined, {}, () => {}, []].forEach((value) => {
+            test(`returns false for "${value}"`, () => {
+                expect(() => hasDeclaredTypeFromElement(value)).toThrowError()
+            });
+        });
+
+        test(`returns when the element has a declared type`, () => {
+            let element = asElement(`<div ${Constants.ATTRIBUTE_TYPE}="sometype"></div>`);
+            expect(hasDeclaredTypeFromElement(element)).toEqual(true);
+        });
+
+        test(`return false when the element has no declared type value`, () => {
+            let element = asElement(`<div ${Constants.ATTRIBUTE_TYPE}></div>`);
+            expect(hasDeclaredTypeFromElement(element)).toEqual(false);
+        });
+
+        test(`return false when the element has no declared type`, () => {
+            let element = asElement(`<div ${Constants.ATTRIBUTE_TYPE}=""></div>`);
+            expect(hasDeclaredTypeFromElement(element)).toEqual(false);
+        });
+
+        test(`return false when the element has an empty declared type`, () => {
+            let element = asElement(`<div ${Constants.ATTRIBUTE_TYPE}="    "></div>`);
+            expect(hasDeclaredTypeFromElement(element)).toEqual(false);
+        });
+
+    });
+
+});
