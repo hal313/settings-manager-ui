3a3b16b19313c6c0e4ace65b963e524ba3947af8
John Ghidiu <jghidiu@solutechnology.com>
2018-07-02 19:19:35 -0400

    Appended .js to imports

--- src/util/isNumber.js
+++ src/util/isNumber.js
@@ -1,7 +1,7 @@
-import { isArray } from './isArray';
-import { isNull } from './isNull';
-import { isEmpty } from './isEmpty';
-import { isBoolean } from './isBoolean';
+import { isArray } from './isArray.js';
+import { isNull } from './isNull.js';
+import { isEmpty } from './isEmpty.js';
+import { isBoolean } from './isBoolean.js';
 
 let isNumber = (value) => {
     return ('number' === typeof value && !isNaN(NaN)) || (!isArray(value) && !isBoolean(value) && !isEmpty(value) && !isNull(value) && !isNaN(+value));
